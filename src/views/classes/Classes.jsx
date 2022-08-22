b4a02b1bcc092cb30dadcb4f824600adb4266133
FarizioKautsar <kheruzy@gmail.com>
2022-08-22 18:34:49 +0700

    Classes list

--- src/views/classes/Classes.jsx
+++ src/views/classes/Classes.jsx
@@ -1,10 +1,25 @@
-import { CButton, CCard, CCardBody, CCardHeader, CDataTable } from '@coreui/react'
+import { CButton, CCard, CCardBody, CCardHeader, CDataTable, CPagination } from '@coreui/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { isLoaded } from 'react-redux-firebase';
 import { Link } from 'react-router-dom'
+import { useFirestorePagination } from 'src/hooks/useFirestorePagination';
+import useQueryString from 'src/hooks/useQueryString';
+import { number, object } from 'yup';
 
 export default function Classes() {
-  const { register } = useForm();
+  const { register, watch } = useForm();
+  const [query] = useQueryString(object().shape({
+    pageSize: number().default(5)
+  }), watch)
+
+  const {
+    list: classes,
+    handlePageChange,
+    page,
+  } = useFirestorePagination("classes", query)
+
+  console.log(classes);
 
   return (
     <CCard>
@@ -20,9 +35,45 @@ export default function Classes() {
         </Link>
       </CCardHeader>
       <CCardBody>
-        <CDataTable>
-
-        </CDataTable>
+        <CDataTable
+          items={classes}
+          fields={[
+            { key: "name", label: "Nama" },
+            { key: "description", label: "Deskripsi" },
+            { key: "actions", label: "" },
+          ]}
+          scopedSlots={{
+            name: (c) => (
+              <td>
+                <Link to={`/classes/${c.id}`}>
+                  {c.name}
+                </Link>
+              </td>
+            ),
+            actions: (c) => (
+              <td className="d-flex justify-content-end">
+                <Link to={`/classes/${c.id}/edit`}>
+                  <CButton
+                    color="primary"
+                    variant="outline"
+                  >
+                    Edit
+                  </CButton>
+                </Link>
+              </td>
+            )
+          }}
+          loading={!isLoaded(classes)}
+        />
+        {
+          classes.length > 5 &&
+          <CPagination
+            activePage={page + 1}
+            onActivePageChange={(newPage) => handlePageChange(Math.max(0, newPage - 1))}
+            pages={0}
+            align="end"
+          />
+        }
       </CCardBody>
     </CCard>
   )
