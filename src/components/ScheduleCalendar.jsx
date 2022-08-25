8ae33ba2ac5a98b2792fa823646bb752a4eb6640
FarizioKautsar <kheruzy@gmail.com>
2022-08-25 21:44:25 +0700

    ScheduleCalendar component

---
+++ src/components/ScheduleCalendar.jsx
@@ -0,0 +1,100 @@
+import { CCol, CLabel, CModal, CModalBody, CModalHeader, CRow } from '@coreui/react';
+import moment from 'moment';
+import React, { useState } from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Link } from 'react-router-dom';
+import { DAY_NUMBERS, SCHEDULE_START_DATE_MS } from 'src/constants';
+import { useGetData } from 'src/hooks/getters';
+import MESpinner from './MESpinner';
+const localizer = momentLocalizer(moment);
+
+export default function ScheduleCalendar(props) {
+  const {
+    events,
+    style = {},
+    onSelectEvent,
+    loading = false
+  } = props;
+
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [classes] = useGetData("classes");
+
+  function handleEventClick(event) {
+    setSelectedEvent(event);
+  }
+
+  function handleCancelEventEdit() {
+    setSelectedEvent(null);
+  }
+
+  const calendarProps = {
+    defaultDate: moment(SCHEDULE_START_DATE_MS).toDate(),
+    toolbar: false,
+    views: [
+      "week"
+    ],
+    defaultView: "week",
+    localizer,
+    style: { ...style, height: "100%" },
+    events,
+    components: {
+      week: {
+        header: ({ date, localizer }) => localizer.format(date, 'dddd')
+      }
+    },
+    onSelectEvent: onSelectEvent || handleEventClick,
+  }
+
+  return (
+    <>
+      <CModal
+        centered 
+        show={Boolean(selectedEvent)}
+        onClose={handleCancelEventEdit}
+      >
+        <CModalHeader className="d-flex justify-content-between">
+          <h4>Detail Jadwal</h4>
+        </CModalHeader>
+        <CModalBody>
+          {
+            selectedEvent && (
+              <CRow>
+                <CCol xs={12}>
+                  <CLabel>Kelas</CLabel>
+                  <Link to={`/classes/${selectedEvent.classId}`}>
+                    <h5>{classes?.[selectedEvent.classId]?.name}</h5>
+                  </Link>
+                </CCol>
+                <CCol xs={12}>
+                  <CLabel>Hari</CLabel>
+                  <h5>{DAY_NUMBERS[selectedEvent.day]}</h5>
+                </CCol>
+                <CCol xs={6}>
+                  <CLabel>Jam Mulai</CLabel>
+                  <h5>{moment(selectedEvent.start).format("HH:mm")}</h5>
+                </CCol>
+                <CCol xs={6}>
+                  <CLabel>Jam Selesai</CLabel>
+                  <h5>{moment(selectedEvent.end).format("HH:mm")}</h5>
+                </CCol>
+                <CCol xs={12}>
+                  <CLabel>Toleransi</CLabel>
+                  <h5>{selectedEvent.tolerance} menit</h5>
+                </CCol>
+              </CRow>
+            )
+          }
+        </CModalBody>
+      </CModal>
+      {
+        loading ? (
+          <MESpinner/>
+        ) : (
+          <Calendar
+            {...calendarProps}
+          />
+        )
+      }
+    </>
+  )
+}
--- src/views/classes/MySchedule.jsx
+++ src/views/classes/MySchedule.jsx
@@ -6,6 +6,7 @@ import { Helmet } from 'react-helmet';
 import { useFirestore, useFirestoreConnect } from 'react-redux-firebase';
 import { Link, useHistory } from 'react-router-dom';
 import MESpinner from 'src/components/MESpinner';
+import ScheduleCalendar from 'src/components/ScheduleCalendar';
 import { DAY_NUMBERS, SCHEDULE_START_DATE_MS } from 'src/constants';
 import { useGetData, useGetProfile, useGetSchoolId } from 'src/hooks/getters';
 const localizer = momentLocalizer(moment);
@@ -15,7 +16,7 @@ export default function MySchedule() {
   const profile = useGetProfile();
 
   const schoolId = useGetSchoolId();
-  const [classes, classesLoading] = useGetData("classes");
+  const [classes] = useGetData("classes");
 
   useFirestoreConnect([
     {
@@ -42,89 +43,17 @@ export default function MySchedule() {
     title: classes?.[schedules[sId].classId]?.name,
   })) : [];
   
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  function handleEventClick(event) {
-    setSelectedEvent(event);
-  }
-
-  function handleCancelEventEdit() {
-    setSelectedEvent(null);
-  }
-
-  const calendarProps = {
-    defaultDate: moment(SCHEDULE_START_DATE_MS).toDate(),
-    toolbar: false,
-    views: [
-      "week"
-    ],
-    defaultView: "week",
-    localizer,
-    style: { height: "100%" },
-    events: schedulesOrdered,
-    components: {
-      week: {
-        header: ({ date, localizer }) => localizer.format(date, 'dddd')
-      }
-    },
-    onSelectEvent: handleEventClick,
-  }
-
   return (
     <div>
-      <CModal 
-        centered 
-        show={Boolean(selectedEvent)}
-        onClose={handleCancelEventEdit}
-      >
-        <CModalHeader className="d-flex justify-content-between">
-          <h4>Detail Jadwal</h4>
-        </CModalHeader>
-        <CModalBody>
-          {
-            selectedEvent && (
-              <CRow>
-                <CCol xs={12}>
-                  <CLabel>Kelas</CLabel>
-                  <Link to={`/classes/${selectedEvent.classId}`}>
-                    <h5>{classes?.[selectedEvent.classId]?.name}</h5>
-                  </Link>
-                </CCol>
-                <CCol xs={12}>
-                  <CLabel>Hari</CLabel>
-                  <h5>{DAY_NUMBERS[selectedEvent.day]}</h5>
-                </CCol>
-                <CCol xs={6}>
-                  <CLabel>Jam Mulai</CLabel>
-                  <h5>{moment(selectedEvent.start).format("HH:mm")}</h5>
-                </CCol>
-                <CCol xs={6}>
-                  <CLabel>Jam Selesai</CLabel>
-                  <h5>{moment(selectedEvent.end).format("HH:mm")}</h5>
-                </CCol>
-                <CCol xs={12}>
-                  <CLabel>Toleransi</CLabel>
-                  <h5>{selectedEvent.tolerance} menit</h5>
-                </CCol>
-              </CRow>
-            )
-          }
-        </CModalBody>
-      </CModal>
       <Helmet>
         <title>Jadwal</title>
       </Helmet>
       <CCard>
         <CCardBody>
-          {
-            schedulesLoading ? (
-              <MESpinner/>
-            ) : (
-              <Calendar
-                {...calendarProps}
-              />
-            )
-          }
+          <ScheduleCalendar
+            events={schedulesOrdered}
+            loading={schedulesLoading}
+          />
         </CCardBody>
       </CCard>
     </div>
