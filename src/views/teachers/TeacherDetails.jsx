3d77e71b277022e91c3effbdc0bb3e89f212b9a8
FarizioKautsar <kheruzy@gmail.com>
2022-08-28 10:28:51 +0700

    TeacherDetails schedule calendar

--- src/views/teachers/TeacherDetails.jsx
+++ src/views/teachers/TeacherDetails.jsx
@@ -1,17 +1,19 @@
-import { CButton, CCard, CCardBody, CCardFooter, CCardHeader, CCol, CRow } from '@coreui/react';
+import { CButton, CCard, CCardBody, CCardFooter, CCardHeader, CCol, CNav, CNavItem, CNavLink, CRow, CTabContent, CTabPane, CTabs } from '@coreui/react';
 import moment from 'moment';
 import React from 'react'
 import { Helmet } from 'react-helmet';
 import { useFirestore, useFirestoreConnect } from 'react-redux-firebase';
 import { Link, useParams } from 'react-router-dom'
 import MESpinner from 'src/components/MESpinner';
-import { useGetAuth, useGetData, useGetProfile } from 'src/hooks/getters';
+import ScheduleCalendar from 'src/components/ScheduleCalendar';
+import { useGetAuth, useGetData, useGetOrdered, useGetProfile, useGetSchoolId } from 'src/hooks/getters';
 
 export default function TeacherDetails() {
   const { teacherId } = useParams();
 
   const firestore = useFirestore();
   const [teacher] = useGetData("users", teacherId);
+  const schoolId = useGetSchoolId();
   useFirestoreConnect([
     {
       collection: "users",
@@ -21,14 +23,39 @@ export default function TeacherDetails() {
       collection: "users",
       where: [[firestore.FieldPath.documentId(), "in", [teacher.createdBy, teacher.updatedBy]]],
       storeAs: "admins"
-    }] : []
+    }] : [],
+    ...teacher?.classIds?.length ? [
+      {
+        collection: "schools",
+        doc: schoolId,
+        subcollections: [{
+          collection: "classes",
+          where: [[firestore.FieldPath.documentId(), "in", teacher.classIds]]
+        }],
+        storeAs: "classes"
+      }, {
+        collectionGroup: "schedules",
+        where: [["classId", "in", teacher.classIds]]
+      }
+    ] : []
   ])
+
+  const [classes, classesLoading] = useGetOrdered("classes", teacher?.classIds);
 
   const [updatedByUser] = useGetData("admins", teacher?.updatedBy);
   const [createdByUser] = useGetData("admins", teacher?.createdBy);
 
   const profile = useGetProfile();
   const auth = useGetAuth();
+
+  const [schedules, schedulesLoading] = useGetData("schedules");
+  const schedulesOrdered = schedules ? Object.keys(schedules).map((sId) => ({ 
+    ...schedules[sId],
+    id: sId, 
+    start: schedules[sId].start.toDate(),
+    end: schedules[sId].end.toDate(),
+    title: classes?.find(({ id }) => id === schedules[sId].classId)?.name,
+  })) : [];
 
   return (
     <CCard>
@@ -56,16 +83,41 @@ export default function TeacherDetails() {
               }
             </CCardHeader>
             <CCardBody>
-              <CRow>
-                <CCol xs={12} md={6}>
-                  <label>Nama Lengkap</label>
-                  <h5>{teacher?.displayName}</h5>
-                </CCol>
-                <CCol xs={12} md={6}>
-                  <label>Nomor ID</label>
-                  <h5>{teacher?.idNumber}</h5>
-                </CCol>
-              </CRow>
+              <CTabs activeTab="details">
+                <CNav variant="tabs">
+                  <CNavItem>
+                    <CNavLink data-tab="details">
+                      Detail
+                    </CNavLink>
+                  </CNavItem>
+                  <CNavItem>
+                    <CNavLink data-tab="schedule">
+                      Jadwal
+                    </CNavLink>
+                  </CNavItem>
+                </CNav>
+                <CTabContent>
+                  <CTabPane data-tab="details">
+                    <CRow className="mt-3">
+                      <CCol xs={12} md={6}>
+                        <label>Nama Lengkap</label>
+                        <h5>{teacher?.displayName}</h5>
+                      </CCol>
+                      <CCol xs={12} md={6}>
+                        <label>Nomor ID</label>
+                        <h5>{teacher?.idNumber}</h5>
+                      </CCol>
+                    </CRow>
+                  </CTabPane>
+                  <CTabPane data-tab="schedule">
+                    <ScheduleCalendar
+                      events={schedulesOrdered}
+                      loading={schedulesLoading}
+                      className="mt-3"
+                    />
+                  </CTabPane>
+                </CTabContent>
+              </CTabs>
             </CCardBody>
             <CCardFooter>
               <small>Dibuat oleh {createdByUser?.displayName} pada {moment(teacher.createdAt.toDate()).format("LLL")}</small>
