4526f6123a0ceb9ec176357874dfff5e7c4701f4
FarizioKautsar <kheruzy@gmail.com>
2022-09-13 22:12:19 +0700

    Dashboard big buttons

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -16,17 +16,101 @@ import CIcon from "@coreui/icons-react";
 
 import MainChartExample from "../charts/MainChartExample.js";
 import { Helmet } from "react-helmet";
+import { FaChalkboardTeacher, FaSchool } from "react-icons/fa";
+import { IoMdSchool } from "react-icons/io";
+import { MdClass, MdVerifiedUser } from "react-icons/md";
+import { AiFillCalendar } from "react-icons/ai";
+import { useGetProfile } from "src/hooks/getters.js";
+import { Link } from "react-router-dom";
 
 const WidgetsDropdown = lazy(() => import("../widgets/WidgetsDropdown.js"));
 const WidgetsBrand = lazy(() => import("../widgets/WidgetsBrand.js"));
 
+const DashboardMenu = props => {
+  const { logo, title, path } = props;
+  return (
+    <Link to={path}>
+      <CButton className="p-0 w-100" color="primary" variant="outline">
+        <CCard
+          className="d-flex align-items-center mb-0 p-4 bg-transparent"
+          style={{ boxShadow: "none" }}
+        >
+          {logo({
+            style: { width: "60", height: "auto" },
+            className: "mb-3"
+          })}
+          <h3>{title}</h3>
+        </CCard>
+      </CButton>
+    </Link>
+  );
+};
+
 const Dashboard = () => {
+  const profile = useGetProfile();
+  const baseMenus = [
+    {
+      logo: FaSchool,
+      title: "Sekolah",
+      path: "/my-school"
+    },
+    {
+      logo: FaChalkboardTeacher,
+      title: "Pengajar",
+      path: "/teachers"
+    },
+    {
+      logo: IoMdSchool,
+      title: "Pelajar",
+      path: "/students"
+    },
+    {
+      logo: MdClass,
+      title: "Kelas",
+      path: "/classes"
+    },
+  ];
+
+  const adminMenus = [
+    {
+      logo: MdVerifiedUser,
+      title: "Administrasi",
+      path: "/admins"
+    }
+  ];
+
+  const teacherMenus = [
+    {
+      logo: AiFillCalendar,
+      title: "Jadwal Kelas",
+      path: "/my-schedule"
+    }
+  ]
+
+  const menus = [
+    ...baseMenus,
+    ...profile.role === "TEACHER" ? teacherMenus : adminMenus
+  ]
+
   return (
     <>
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
-      <WidgetsDropdown />
+
+      <CRow>
+        {menus.map(menu => (
+          <CCol xs={6} md={3} className="mb-4">
+            <DashboardMenu
+              logo={menu.logo}
+              title={menu.title}
+              path={menu.path}
+            />
+          </CCol>
+        ))}
+      </CRow>
+
+      {/* <WidgetsDropdown />
       <CCard>
         <CCardBody>
           <CRow>
@@ -703,7 +787,7 @@ const Dashboard = () => {
             </CCardBody>
           </CCard>
         </CCol>
-      </CRow>
+      </CRow> */}
     </>
   );
 };
