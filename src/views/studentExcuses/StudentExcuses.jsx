12ed784c78edceff16d2569e2c1a1f89118e1202
FarizioKautsar <kheruzy@gmail.com>
2022-12-10 13:41:00 +0700

    StudentExcuses refreshes after change

--- src/views/studentExcuses/StudentExcuses.jsx
+++ src/views/studentExcuses/StudentExcuses.jsx
@@ -1,8 +1,9 @@
 import { CButton, CCard, CCardBody, CCardHeader, CDataTable, CInputCheckbox, CLabel } from '@coreui/react'
 import moment from 'moment';
 import React, { useState } from 'react'
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useFirestoreConnect } from 'react-redux-firebase'
+import { useFirestore, useFirestoreConnect } from 'react-redux-firebase'
 import { Link } from 'react-router-dom';
 import meConfirm from 'src/components/meConfirm';
 import MESelect from 'src/components/MESelect';
@@ -14,41 +15,54 @@ import { changeSchoolLogExcuseStatus } from 'src/store/actions/scheduleActions';
 export default function StudentExcuses() {
   const schoolId = useGetSchoolId();
   const dispatch = useDispatch();
-  const [waitingOnly, setWaitingOnly] = useState(false);
+  const firestore = useFirestore();
+  const [waitingOnly, setWaitingOnly] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [combinedLogs, setStudentLogs] = useState([]);
 
+  async function getStudentLogs() {
+    setStudentLogs([]);
+    const schoolLogsRef = firestore
+      .collection("schools")
+      .doc(schoolId)
+      .collection("logs")
+      .where("status", "==", "EXCUSED")
+    const schoolStudentLogsRef = firestore
+      .collectionGroup("studentLogs")
+      .where("status", "==", "EXCUSED")
+      .where("schoolId", "==", schoolId)
+    if (waitingOnly) {
+      schoolLogsRef.where("excuseStatus", "==", "WAITING");
+      schoolStudentLogsRef.where("excuseStatus", "==", "WAITING");
+    }
+    schoolLogsRef.orderBy("time", "desc");
+    schoolStudentLogsRef.orderBy("time", "desc");
+    setIsLoading(true);
+    const studentLogSnaps = await schoolStudentLogsRef.get();
+    const logSnaps = await schoolLogsRef.get();
+    setStudentLogs([...studentLogSnaps.docs, ...logSnaps.docs].map((doc) => ({
+      ...doc.data(),
+      id: doc.id
+    })));
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getStudentLogs();
+  }, [waitingOnly])
+ 
   useFirestoreConnect([
-    {
-      collectionGroup: "logs",
-      where: [
-        ["schoolId", "==", schoolId],
-        ["status", "==", "EXCUSED"],
-        ...waitingOnly ? [["excuseStatus", "==", "WAITING"]] : []
-      ],
-      orderBy: ["time", "desc"]
-    },
-    {
-      collectionGroup: "studentLogs",
-      where: [
-        ["schoolId", "==", schoolId],
-        ["status", "==", "EXCUSED"],
-        ...waitingOnly ? [["excuseStatus", "==", "WAITING"]] : []
-      ],
-      orderBy: ["time", "desc"]
-    },
     {
       collection: "users",
       where: ["schoolId", "==", schoolId]
     }
   ])
   const [students] = useGetData("users");
-  const [ logsState, logsLoading ] = useGetData("logs");
-  const [ studentLogsState, studentLogsLoading ] = useGetData("studentLogs");
-  const logs = Object.entries(studentLogsState || {}).concat(Object.entries(logsState || {}));
-  const modifiedLogs = logs?.map((l) => ({ id: l[0], ...l[1], studentName: students?.[l[1].studentId]?.displayName }));
+  const modifiedLogs = combinedLogs?.map((l) => ({ ...l, studentName: students?.[l[1]?.studentId]?.displayName }));
 
   const [status, setStatus] = useState("");
   const [selectedLogId, setSelectedLogId] = useState(null);
-  const selectedLog = { ...logsState, ...studentLogsState }?.[selectedLogId];
+  const selectedLog = combinedLogs.find((l) => l.id === selectedLogId);
 
   function handleSelectLog(logId) {
     setSelectedLogId(logId);
@@ -67,6 +81,7 @@ export default function StudentExcuses() {
             .then(() => {
               setSelectedLogId(null);
               setExcuseStatusLoading(null);
+              getStudentLogs();
             })
         }
       }
@@ -112,7 +127,7 @@ export default function StudentExcuses() {
             time: <></>,
             actions: <></>
           }}
-          loading={logsLoading || studentLogsLoading}
+          loading={isLoading}
           fields={[
             { key: "studentName", label: "Nama Pelajar" },
             { key: "time", label: "Tanggal & Waktu Kehadiran" },
@@ -136,7 +151,7 @@ export default function StudentExcuses() {
             ),
             time: (l) => (
               <td>
-                {moment(l.time.toDate()).format("LLL")}
+                {moment(l.time?.toDate()).format("LLL")}
               </td>
             ),
             actions: (l) => (
